717265f39ed2b420d143705a61e61a2bfa04b26c
Caleb Hodges <chodgesdev@gmail.com>
2023-05-11 17:35:09 -0500

    Part 1 complete

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import AuthContext from "../store/authContext";
+
+import axios from "axios";
 
 const Auth = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [register, setRegister] = useState(true);
 
+  const authCtx = useContext(AuthContext);
+
   const submitHandler = (e) => {
     e.preventDefault();
 
-    console.log("submitHandler called");
+    const body = {
+      username,
+      password,
+    };
+
+    const url = "https://socialmtn.devmountain.com";
+
+    axios
+      .post(register ? `${url}/register` : `${url}/login`, body)
+      .then(({ data }) => {
+        console.log("AFTER AUTH", data);
+        authCtx.login(data.token, data.exp, data.userId)
+      })
+      .catch((err) => {
+        setPassword("");
+        setUsername("");
+      });
   };
 
   const usernameChangeHandler = (e) => {
